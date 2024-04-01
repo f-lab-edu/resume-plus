7862e373693ba40551f978b74616d304b70e3f88
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-04-01 14:12:56 +0900

    feat : User model

---
+++ src/main/java/com/ddoddii/resume/model/User.java
@@ -0,0 +1,44 @@
+package com.ddoddii.resume.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.List;
+import lombok.Data;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Resume> resumes;
+}
