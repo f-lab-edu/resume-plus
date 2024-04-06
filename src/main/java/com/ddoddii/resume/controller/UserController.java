63e6e646360c250d3f82369962b9763b23406432
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-04-06 16:37:52 +0900

    feat : 유저서비스 레이어 기능 추가
    - 유저 삭제
    - 유저 비밀번호 변경
    - 컨트롤러 로직 서비스 레이어로 옮김

--- src/main/java/com/ddoddii/resume/controller/UserController.java
+++ src/main/java/com/ddoddii/resume/controller/UserController.java
@@ -1,20 +1,11 @@
 package com.ddoddii.resume.controller;
 
-import com.ddoddii.resume.dto.UserLoginRequestDto;
-import com.ddoddii.resume.dto.UserRegisterDto;
-import com.ddoddii.resume.model.RoleType;
-import com.ddoddii.resume.model.User;
+import com.ddoddii.resume.dto.ResultDTO;
+import com.ddoddii.resume.dto.UserDTO;
 import com.ddoddii.resume.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,52 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-    private final AuthenticationManager authenticationManager;
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-
-    @Autowired
-    public UserController(UserService userService, AuthenticationManager authenticationManager) {
-        this.userService = userService;
-        this.authenticationManager = authenticationManager;
+    @PostMapping
+    public ResponseEntity<ResultDTO<String>> signUp(@RequestBody UserDTO user) {
+        userService.signUp(user);
+        return ResponseEntity.ok(
+                ResultDTO.res(HttpStatus.CREATED, HttpStatus.CREATED.toString(), "User created successfully"));
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserRegisterDto userRegisterDto) {
-        try {
-            User user = new User();
-            user.setUsername(userRegisterDto.getUsername());
-            user.setEmail(userRegisterDto.getEmail());
-            user.setPassword(userRegisterDto.getPassword());
-            user.setRole(RoleType.USER);
-
-            logger.info("Registering user: {}", user.getUsername());
-
-            userService.registerUser(user);
-            logger.info("User registered successfully: {}", user.getUsername());
-            return ResponseEntity.ok("User registered successfully");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody UserLoginRequestDto loginRequest) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            loginRequest.getUsername(),
-                            loginRequest.getPassword()
-                    )
-            );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            // Generate and return JWT token or session token
-            return ResponseEntity.ok("User logged in successfully");
-        } catch (AuthenticationException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-        }
-    }
 }
--- src/main/java/com/ddoddii/resume/service/UserService.java
+++ src/main/java/com/ddoddii/resume/service/UserService.java
@@ -1,32 +1,74 @@
 package com.ddoddii.resume.service;
 
+import com.ddoddii.resume.dto.UserDTO;
+import com.ddoddii.resume.error.errorcode.UserErrorCode;
+import com.ddoddii.resume.error.exception.DuplicateIdException;
+import com.ddoddii.resume.error.exception.NotExistIdException;
+import com.ddoddii.resume.model.RoleType;
 import com.ddoddii.resume.model.User;
 import com.ddoddii.resume.repository.UserRepository;
+import com.ddoddii.resume.util.PasswordEncrypter;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/*
+사용자의 회원가입, 로그인을 담당하는 서비스 레이어
+ */
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public void registerUser(User user) {
-        if (userRepository.existsByUsername(user.getUsername())) {
-            throw new IllegalArgumentException("Username already exists");
+    public void signUp(UserDTO userDTO) {
+        if (userRepository.existsByUserId(userDTO.getUserId())) {
+            throw new DuplicateIdException(UserErrorCode.DUPLICATE_USER);
         }
 
-        if (userRepository.existsByEmail(user.getEmail())) {
-            throw new IllegalArgumentException("Email already exists");
-        }
-        
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        User encryptedUser = encryptUser(userDTO);
 
+        userRepository.save(encryptedUser);
+    }
+
+    private User encryptUser(UserDTO userDTO) {
+        String encryptedPassword = PasswordEncrypter.encrypt(userDTO.getPassword());
+        User user = new User();
+        user.setUserId(userDTO.getUserId());
+        user.setPassword(encryptedPassword);
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setRole(RoleType.USER);
+        user.setCreatedAt(LocalDateTime.now());
+        return user;
+    }
+
+    public void deleteUser(String userId) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new NotExistIdException(UserErrorCode.NOT_EXIST_USER));
+        userRepository.delete(user);
+    }
+
+    public void changeUserPassword(String userId, String newPassword) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new NotExistIdException(UserErrorCode.NOT_EXIST_USER));
+        String newEncryptedPassword = PasswordEncrypter.encrypt(newPassword);
+        user.setPassword(newEncryptedPassword);
         userRepository.save(user);
     }
 
+    public Optional<UserDTO> findUserByIdAndPassword(String id, String password) {
+        return userRepository.findByUserId(id)
+                .filter(user -> PasswordEncrypter.isMatch(password, user.getPassword()))
+                .map(this::convertToDto);
+    }
+
+    private UserDTO convertToDto(User user) {
+        return UserDTO.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+    }
 }
