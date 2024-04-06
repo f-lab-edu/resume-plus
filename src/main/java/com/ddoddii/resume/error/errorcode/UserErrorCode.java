f86a9393bcb9a21714cfc6c74d8389e3b1afe67a
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-04-06 16:34:57 +0900

    feat : error code , custom exception 추가

---
+++ src/main/java/com/ddoddii/resume/error/errorcode/UserErrorCode.java
@@ -0,0 +1,16 @@
+package com.ddoddii.resume.error.errorcode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+    DUPLICATE_USER(HttpStatus.CONFLICT, "User with same id already exists"),
+    NOT_EXIST_USER(HttpStatus.NOT_FOUND, "User not found");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
