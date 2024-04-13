2fce52f02bf683d400c5295df9218b4e501d2477
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-04-13 23:53:04 +0900

    feat : Repository 생

---
+++ src/main/java/com/ddoddii/resume/repository/InterviewRepository.java
@@ -0,0 +1,9 @@
+package com.ddoddii.resume.repository;
+
+import com.ddoddii.resume.model.Interview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+}
