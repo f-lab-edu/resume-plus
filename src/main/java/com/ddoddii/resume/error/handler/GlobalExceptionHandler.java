eddd7f09b988ebfdf8eae23ac4fdfc84729e4f2f
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-04-14 15:32:34 +0900

    feat : 로그인 인증 관련 커스텀 예외 생성

--- src/main/java/com/ddoddii/resume/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ddoddii/resume/error/handler/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.ddoddii.resume.error.handler;
 
 import com.ddoddii.resume.error.ErrorResponse;
 import com.ddoddii.resume.error.errorcode.ErrorCode;
+import com.ddoddii.resume.error.exception.BadCredentialsException;
 import com.ddoddii.resume.error.exception.DuplicateIdException;
+import com.ddoddii.resume.error.exception.NotExistIdException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +16,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DuplicateIdException.class)
     public ResponseEntity<Object> handleDuplicateUserRequest(final DuplicateIdException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return handleExceptionInternal(errorCode);
+    }
+
+    @ExceptionHandler(NotExistIdException.class)
+    public ResponseEntity<Object> handleNotExistId(final NotExistIdException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return handleExceptionInternal(errorCode);
+    }
+
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Object> handleBadCredentials(final BadCredentialsException e) {
         final ErrorCode errorCode = e.getErrorCode();
         return handleExceptionInternal(errorCode);
     }
