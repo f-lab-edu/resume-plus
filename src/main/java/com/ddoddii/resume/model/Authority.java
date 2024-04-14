ca7463253e079d05eedbc361a2ebf684ef56a9ce
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-04-14 15:32:58 +0900

    feat : Authority 엔티티 생성

---
+++ src/main/java/com/ddoddii/resume/model/Authority.java
@@ -0,0 +1,35 @@
+package com.ddoddii.resume.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    private String name;
+
+    @JoinColumn(name = "user")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
