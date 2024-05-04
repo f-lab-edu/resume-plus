e5c03db407033810088216116272689aa083a13f
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-05-04 17:24:35 +0900

    feat : refreshToken 모델, 레포지토리, DTO 생성

---
+++ src/main/java/com/ddoddii/resume/dto/RefreshTokenRequestDTO.java
@@ -0,0 +1,12 @@
+package com.ddoddii.resume.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RefreshTokenRequestDTO {
+    private String token;
+}
