a8b2983896ffeae5f900c0d504e1ff18328d5d43
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-05-04 17:26:43 +0900

    feat : refreshToken 발급하고 DB에 저장

--- src/main/java/com/ddoddii/resume/controller/UserController.java
+++ src/main/java/com/ddoddii/resume/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.ddoddii.resume.controller;
 
+import com.ddoddii.resume.dto.JwtTokenDTO;
+import com.ddoddii.resume.dto.RefreshTokenRequestDTO;
 import com.ddoddii.resume.dto.UserLoginRequestDTO;
 import com.ddoddii.resume.dto.UserLoginResponseDTO;
 import com.ddoddii.resume.dto.UserSignUpRequestDTO;
 import com.ddoddii.resume.dto.UserSignUpResponseDTO;
+import com.ddoddii.resume.security.TokenProvider;
+import com.ddoddii.resume.service.RefreshTokenService;
 import com.ddoddii.resume.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<UserSignUpResponseDTO> signUp(@RequestBody @Valid UserSignUpRequestDTO user) {
@@ -30,6 +36,12 @@ public class UserController {
     public ResponseEntity<UserLoginResponseDTO> login(@RequestBody @Valid UserLoginRequestDTO request) {
         UserLoginResponseDTO response = userService.login(request);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtTokenDTO> refreshToken(@RequestBody RefreshTokenRequestDTO refreshTokenRequestDTO) {
+        JwtTokenDTO jwtTokenDTO = userService.generateNewAccessToken(refreshTokenRequestDTO.getToken());
+        return ResponseEntity.ok(jwtTokenDTO);
     }
 
 }
