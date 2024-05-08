f1923783d213674d3b3c1f6e8a11c0cd0d35a4ef
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-05-08 18:09:20 +0900

    refactor : 권한 RoleType 으로 통일

--- src/main/java/com/ddoddii/resume/model/User.java
+++ src/main/java/com/ddoddii/resume/model/User.java
@@ -1,12 +1,10 @@
 package com.ddoddii.resume.model;
 
 import com.ddoddii.resume.model.eunm.RoleType;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,7 +12,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -53,14 +50,12 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @NotNull
     private RoleType role;
-    
+
     @OneToMany(mappedBy = "user")
     private List<Resume> resumes;
 
     @OneToMany(mappedBy = "user")
     private List<Interview> interviews;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @Builder.Default
-    private List<Authority> roles = new ArrayList<>();
+
 }
