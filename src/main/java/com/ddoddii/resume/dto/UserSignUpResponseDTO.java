6ab194998ee32dedf91a00127174d0ea314a0877
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-05-10 17:02:20 +0900

    chore : ToString 어노테이션 추가

--- src/main/java/com/ddoddii/resume/dto/UserSignUpResponseDTO.java
+++ src/main/java/com/ddoddii/resume/dto/UserSignUpResponseDTO.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class UserSignUpResponseDTO {
     private String name;
     private String email;
