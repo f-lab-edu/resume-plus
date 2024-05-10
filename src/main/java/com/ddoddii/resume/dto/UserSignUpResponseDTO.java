af654d25407750d019dcd0fe175061a2783c83ea
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-05-10 16:59:08 +0900

    feat : Resume 업로드 시 응답 DTO 생성

--- src/main/java/com/ddoddii/resume/dto/UserSignUpResponseDTO.java
+++ src/main/java/com/ddoddii/resume/dto/UserSignUpResponseDTO.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class UserSignUpResponseDTO {
     private String name;
     private String email;
